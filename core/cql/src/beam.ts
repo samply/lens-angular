e7f3a76e2618449637d54f09b323e6a1bf806e70
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2023-09-25 08:28:22 +0200

    feat: modified measure structur for other projects
    BREAKING CHANGE: old measure are not useable anymore and need to adapt to the new interface

--- core/cql/src/beam.ts
+++ core/cql/src/beam.ts
@@ -49,6 +49,10 @@ export class Beam implements RequestTarget {
   async send(query: string, measures: Object[]): Promise<string> {
     this.resultSubject$.next(new Map<string, any>())
 
+    console.log("Send")
+    console.log(query)
+    console.log(measures)
+
     let baseCQL = btoa(unescape(encodeURIComponent(query)));
 
     let libUUID = uuidv4();
