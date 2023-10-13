06fbacbd5ecb5ecb67aec86983b2018ad3bc1f39
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-10-13 06:42:14 +0000

    fix: remove debug logs

--- core/src/lib/services/query.service.ts
+++ core/src/lib/services/query.service.ts
@@ -68,9 +68,7 @@ export class QueryService {
   public async send() {
     // NOTE: In addition to this reset, each request target implementation needs to reset their results (see blaze.ts and beam.ts)
     this.resultsSubject$.next(new Map<string, any>())
-    console.log(this.querySubject$.value)
     let transformedQuery = this.queryTranslator.transform(this.querySubject$.value.ast);
-    console.log(transformedQuery)
     this.querySubject$.value.tasks = await Promise.all(
       this.configuration.requestTargets
       // create instance of requestTarget
