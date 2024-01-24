3d79fd714b9e1bfbc35da2d13b20a5b8058117c9
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-01-24 11:39:38 +0100

    feat: added new dhki search parameters

--- core/src/lib/services/query.service.ts
+++ core/src/lib/services/query.service.ts
@@ -69,6 +69,7 @@ export class QueryService {
     // NOTE: In addition to this reset, each request target implementation needs to reset their results (see blaze.ts and beam.ts)
     this.resultsSubject$.next(new Map<string, any>())
     let transformedQuery = this.queryTranslator.transform(this.querySubject$.value.ast);
+    console.log(transformedQuery)
     this.querySubject$.value.tasks = await Promise.all(
       this.configuration.requestTargets
       // create instance of requestTarget
