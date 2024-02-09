ceac14d14c623f6980153c6bf737b30ecc88a940
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-02-09 15:35:33 +0100

    test: added dependencies for query services

--- components/result-renderer-grid/src/result-renderer-grid.directive.spec.ts
+++ components/result-renderer-grid/src/result-renderer-grid.directive.spec.ts
@@ -1,13 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResultRendererGridComponent } from './result-renderer-grid.component';
+import {
+  CATALOGUE_FETCHER_TOKEN,
+  Category,
+  ChipTransformPipe,
+  LensConfig,
+  LENS_CONFIG_TOKEN,
+  QUERY_TRANSLATOR_TOKEN,
+  RESULT_TRANSFORMER_TOKEN,
+  STATIC_CATALOGUE_TOKEN,
+  TypescriptCatalogueFetcherService
+} from '@samply/lens-core';
 
 import { ResultRendererGridDirective } from './result-renderer-grid.directive';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+const STATIC_CATALOGUE: Array<Category> = [];
 
 describe('ResultRendererGridDirective', () => {
   let fixture: ComponentFixture<ResultRendererGridComponent>;
   beforeEach(async () => {
+    let queryTranslatorSpy = jasmine.createSpyObj(['transform']);
+    let resultTransformerSpy = jasmine.createSpyObj(['transform']);
     await TestBed.configureTestingModule({
       declarations: [ResultRendererGridDirective, ResultRendererGridComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{
+        provide: QUERY_TRANSLATOR_TOKEN,
+        useValue: queryTranslatorSpy
+      },{
+        provide: RESULT_TRANSFORMER_TOKEN,
+        useValue: resultTransformerSpy
+      },{
+        provide: LENS_CONFIG_TOKEN,
+        useValue: new LensConfig()
+      },{
+        provide: ChipTransformPipe
+      },{
+        provide: CATALOGUE_FETCHER_TOKEN,
+        useClass: TypescriptCatalogueFetcherService
+      },{
+        provide: STATIC_CATALOGUE_TOKEN,
+        useValue: STATIC_CATALOGUE
+      }]
     }).compileComponents();
     fixture = TestBed.createComponent(ResultRendererGridComponent);
   })
